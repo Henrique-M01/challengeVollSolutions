36ad93848a7b888955f2e325256745b16eab1c61
henrique <hickm018@gmail.com>
2022-05-06 19:33:05 -0300

    refactor: change password using bcrypt

--- src/database/seeders/20220506215837-seedUsers.js
+++ src/database/seeders/20220506215837-seedUsers.js
@@ -6,19 +6,19 @@ module.exports = {
       {
         name: 'John Doe',
         email: 'johndoe@email.com',
-        password: 'secret_password',
+        password: '$2a$10$hivelJBMidKzG4r1NTase.c08xjzEjEf2eUXsiGQFnxd83ZWIU.eO', //secret_password
         role: 'user'
       },
       {
         name: 'Henrique Silva',
         email: 'silvahenrique@email.com',
-        password: 'secret_silva',
+        password: '$2a$12$GTlJdM8tGJs8eAyOFtQADeem7VqeYZRh6hjwQPGwIGJWKHxdNcccO', //secret_silva
         role: 'admin'
       },
       {
         name: 'Carol Santos',
         email: 'santos@email.com',
-        password: 'santos_password',
+        password: '$2a$12$5gMrz9SWw10ZUySxWXvw9OpdPOB6skZTcczotOGbn9hvrpvkFtV82', //santos_password
         role: 'user'
       },
     ], { timestamps: false });
