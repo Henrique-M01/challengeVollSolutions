554ecf12011362dc8e2ccbd0d4ed8d903b8a3420
henrique <hickm018@gmail.com>
2022-05-10 01:02:17 -0300

    refact: rename error middleware and enable

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddlewares';
 import indexRouter from './Routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(indexRouter)
+app.use(errorMiddleware)
 
 export default app;
